d40c1bf55894235e313999c3cfeb6daaf69abf31
Enrique <Enrique.Metner@haw-hamburg.de>
2024-04-16 23:24:36 +0200

    implemented accumulators

---
+++ app/src/main/java/adp/aufgabe1/teilB/AccumulatorClient.java
@@ -0,0 +1,62 @@
+package adp.aufgabe1.teilB;
+
+import java.util.Scanner;
+
+public class AccumulatorClient {
+    
+    public static void main(String[] args) {
+        SimpleAccumulator accumulator;
+        try {
+            int firstArg = Integer.parseInt(args[0]);
+            int trials = Integer.parseInt(args[1]);
+            double maxValue = Double.parseDouble(args[2]);
+            if (trials <= 0) {
+                System.out.println("Please provide a positive integer as argument.");
+                return;
+            }
+            if (maxValue <= 0) {
+                System.out.println("Please provide a positive number as argument.");
+                return;
+            }
+
+            if (firstArg == 0) {
+                accumulator = new SimpleAccumulator();
+            }
+            else if (firstArg > 0) {
+                accumulator = new VisualAccumulator(trials, maxValue);
+            } else {
+                System.out.println("Please provide a positive number as argument.");
+                return;
+                
+            }
+            
+            readNumbers(accumulator, trials);
+        } catch (NumberFormatException e) {
+            System.out.println("Please provide a valid number as argument.");
+            return;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please provide a number as argument.");
+            return;
+        }
+        
+    }
+
+    private static void readNumbers(SimpleAccumulator accumulator, int trials) {
+        Scanner scanner = new Scanner(System.in);
+
+        int i = 0;
+        while (i++ < trials && scanner.hasNext()) {
+            double value = scanner.nextDouble();
+            accumulator.addDataValue(value);
+        }
+        
+        System.out.println("mean: " + accumulator.mean());
+        scanner.close();
+    }
+
+    public static void main1(String[] args) {
+        String[] arg = {"1", "5", "10"};
+        main1(arg);
+    }
+
+}
---
+++ app/src/main/java/adp/aufgabe1/teilB/VisualAccumulator.java
@@ -0,0 +1,36 @@
+package adp.aufgabe1.teilB;
+
+import edu.princeton.cs.algs4.StdDraw;
+import java.awt.Color;
+import java.util.Scanner;
+
+public class VisualAccumulator extends SimpleAccumulator {
+    public VisualAccumulator(int trials, double max){
+        StdDraw.setXscale(0,trials);
+        StdDraw.setYscale(0,max);
+        StdDraw.setPenRadius(0.005);
+        }
+
+        @Override
+        public void addDataValue(double d) {
+            n++;
+            sum += d;
+            StdDraw.setPenColor(Color.DARK_GRAY);
+            StdDraw.point(n, d);
+            StdDraw.setPenColor(Color.RED);
+            StdDraw.point(n, sum/n);
+        }
+
+        public static void main(String[] args) {
+            VisualAccumulator accumulator = new VisualAccumulator(5, 5);
+            Scanner scanner = new Scanner(System.in);
+            int i = 0;
+            while (i++ < 5 && scanner.hasNext()) {
+                double value = scanner.nextDouble();
+                accumulator.addDataValue(value);
+            }
+            System.out.println("mean: " + accumulator.mean());
+            scanner.close();
+        }
+    }
+
